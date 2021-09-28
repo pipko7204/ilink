56df81bf005863336d000ba3b7255b8629da7eb9
Pavel Mamatov <pmamatov@icloud.com>
2021-09-29 01:38:00 +0300

    test

---
+++ index.js
@@ -0,0 +1,30 @@
+let form = document.querySelector('form')
+
+let name = form.querySelector('.name');
+let iname = form.querySelector('.iname');
+let gender = form.querySelector('.gender');
+let igender = form.querySelector('.igender');
+let country = form.querySelector('.country');
+let icountry = form.querySelector('.icountry');
+let city = form.querySelector('.city');
+let icity = form.querySelector('.icity');
+let birth = form.querySelector('.birth');
+let ibirth = form.querySelector('.ibirth');
+let file = form.querySelector('.file');
+let checkComplete = form.querySelector('.checkComplete');
+let input = form.querySelectorAll('input');
+
+console.log('###: form', form);
+console.log('###: input', input);
+
+
+let inviElements = [country, icountry, city, icity, birth, ibirth, file, checkComplete];
+
+setTimeout("alert('я пытался   ¯|_(ツ)_/¯ ') ", 10000)
+
+/*for (let i = 0; i < inviElements.length; i++) {
+    inviElements[i].classList.add('hidden')
+}*/
+
+
+
